943b37733afd6a466884ef34a55774c1fd02b841
Daniel Escalona <daniel07escalona@gmail.com>
2022-04-12 14:17:05 -0400

    commit tree f10b591863b1128da31d5b36280668d215e759fb

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+
+  const [plants, setPlants] = useState([])
+  const [backEnd, setBackEnd] = useState([])
+  useEffect( () => {
+    fetch('http://localhost:6001/plants')
+      .then( data => data.json() )
+      .then( plants => setPlants(plants))
+  }, [backEnd])
+
+  function addNewPlant(item){
+    setPlants([...plants, item])
+  }
+
+  function filterPlants(input){
+
+    if(input === ''){
+      setBackEnd([...backEnd])
+    }
+    const filteredPlants = plants.filter( plantObj => {
+      let name = plantObj.name.toLowerCase()
+      return name.includes(input.toLowerCase()) ? plantObj: false
+    })
+    setPlants(filteredPlants)
+  }
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} onSubmit={addNewPlant} onSearch={filterPlants}/>
     </div>
   );
 }
